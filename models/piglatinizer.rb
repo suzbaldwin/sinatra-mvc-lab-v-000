1d897f1ad861cc5417db334e41b434141266f762
suzy baldwin <gbaldwin24@comcast.net>
2018-03-17 18:39:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,8 +2,64 @@ class PigLatinizer
 
 
   def piglatinize(word)
-  
+    vowels = ["a", "e", "i", "o", "u"]
 
-  end
+      # Special cases to consider
+      two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
+      three_letter_consonants = ["thr", "sch", "squ"]
+
+      # Seperate each word from the phrase given
+      words = str.split(" ")
+
+      #Location for processed words
+      result = [];
+
+
+
+      words.each do |word|
+          # Words that start with a vowels
+          if vowels.include? word[0]
+              result.push word << 'ay'
+
+          # Words that start with a consonant
+          else
+              # Check for special consonants
+              if three_letter_consonants.include? word[0] + word[1] + word[2]
+                  # Slice off first three letters
+                  first_three_letters = word.slice!(0,3)
+
+                  # Add letters to end of word with 'ay'
+                  result.push word << first_three_letters << 'ay'
+
+              elsif  two_letter_consonants.include? word[0] + word[1]
+                  # Slice off first two letters
+                  first_two_letters = word.slice!(0,2)
+
+                  # Add the letters to end of word with 'ay'
+                  result.push word << first_two_letters << 'ay'
+
+              else
+                  # Slice off first letter...
+                  first_letter = word.slice!(0)
+
+                  # Add first letter to end of word with 'ay'
+                  result.push word << first_letter << 'ay'
+              end #End of special consonant check
+
+          end #End of vowel check
+
+
+
+      end #End of words.each
+
+
+
+      #Present the processed words as a single string
+      return result.join(" ")
+
+  end #End of translate function
+
+
+  
 
  end
