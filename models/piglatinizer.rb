19a101b548bedc38fb04361eaa5bbb6df8f0ecfb
suzy baldwin <gbaldwin24@comcast.net>
2018-03-17 18:42:27 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,65 +1,18 @@
 class PigLatinizer
-
+  VOWELS = /a|e|i|o|u/i
 
   def piglatinize(word)
-    vowels = ["a", "e", "i", "o", "u"]
 
-      # Special cases to consider
-      two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
-      three_letter_consonants = ["thr", "sch", "squ"]
+   
+     if self[0] =~ VOWELS
+       self + 'yay'
+     else
+       pieces = self.split(VOWELS)
+       first_consonant = pieces.first
+       self[first_consonant.length..-1] + first_consonant + 'ay'
+     end
+   end
 
-      # Seperate each word from the phrase given
-      words = word.split(" ")
-
-      #Location for processed words
-      result = [];
-
-
-
-      words.each do |word|
-          # Words that start with a vowels
-          if vowels.include? word[0]
-              result.push word << 'ay'
-
-          # Words that start with a consonant
-          else
-              # Check for special consonants
-              if three_letter_consonants.include? word[0] + word[1] + word[2]
-                  # Slice off first three letters
-                  first_three_letters = word.slice!(0,3)
-
-                  # Add letters to end of word with 'ay'
-                  result.push word << first_three_letters << 'ay'
-
-              elsif  two_letter_consonants.include? word[0] + word[1]
-                  # Slice off first two letters
-                  first_two_letters = word.slice!(0,2)
-
-                  # Add the letters to end of word with 'ay'
-                  result.push word << first_two_letters << 'ay'
-
-              else
-                  # Slice off first letter...
-                  first_letter = word.slice!(0)
-
-                  # Add first letter to end of word with 'ay'
-                  result.push word << first_letter << 'ay'
-              end #End of special consonant check
-
-          end #End of vowel check
-
-
-
-      end #End of words.each
-
-
-
-      #Present the processed words as a single string
-      return result.join(" ")
-
-  end #End of translate function
-
-
-
+  end
 
  end
