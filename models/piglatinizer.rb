c7323635840a522a4543f84ffded93f8eaca4aea
suzy baldwin <gbaldwin24@comcast.net>
2018-03-17 18:46:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,14 +4,34 @@ class PigLatinizer
   def piglatinize(word)
 
 
-     if word =~ VOWELS
-       self + 'yay'
-     else
-       pieces = self.split(VOWELS)
-       first_consonant = pieces.first
-       self[first_consonant.length..-1] + first_consonant + 'ay'
-     end
-   end
+    words = word.split(' ')
+	word = word.split('')
+	trans = []
+	x = word.dup
+
+
+	if words.size > 1
+		words.map do |phrase|
+			phrase.split('').map do |letter|
+				break if %w(a e i o u).include?(letter)
+				trans << letter
+				if phrase.first != letter
+					x.shift
+				end
+				puts
+				print "this is trans: #{trans}"
+			end
+		end
+		x.join + trans.join + "ay"
+
+	else
+		word.map do |letter|
+			break if %w(a e i o u).include?(letter)
+			trans << letter
+			x.shift
+		end
+		x.join + trans.join + "ay"
+	end
 
 
 
